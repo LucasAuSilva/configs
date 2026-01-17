bfcba1f0d91f5960b9c41113092b023994a56a15
LucasAuSilva <silvaaugustolucas@gmail.com>
2026-01-16 22:38:12 -0300

    chore: make browser understand dark mode

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,7 @@
 set -Ux PATH $PATH $HOME/go/bin
 
+set -x XDG_CURRENT_DESKTOP Hyprland
+set -x GTK_THEME Adwaita:dark
+
 # Generated for envman. Do not edit.
 test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
