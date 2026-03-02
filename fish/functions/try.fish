6f085fdf9fe33fcf7a705b8d29b819703a331040
LucasAuSilva <silvaaugustolucas@gmail.com>
2026-03-01 21:46:15 -0300

    fix: try command was not function as should be

--- fish/functions/try.fish
+++ fish/functions/try.fish
@@ -1,28 +1,22 @@
 #!/usr/bin/env fish
 
 function try
-    set script ~/.local/try.rb
-    set tries_dir ~/src/tries
+    set -l script ~/.local/try.rb
+    set -l tries_dir ~/src/tries
 
-    if not test -x $script
-        echo "try: script not found or not executable: $script" >&2
+    if not test -f $script
+        echo "try: script not found: $script" >&2
         return 1
     end
 
-    # Run try.rb and capture output
-    set cmd (ruby $script cd --path $tries_dir $argv 2>/dev/tty)
-    set status_code $status
+    # Run in exec mode so it emits shell script
+    set -l out (ruby $script exec --path $tries_dir $argv 2>/dev/tty | string collect)
+    set -l status_code $pipestatus[1]
 
-    if test $status_code -ne 0
-        echo $cmd
+    if test $status_code -eq 0
+        eval $out
+    else
+        echo $out
         return $status_code
     end
-
-    # If the command contains shell operators, eval it
-    if string match -q "* && *" -- $cmd
-        eval $cmd
-    else
-        echo $cmd
-    end
 end
-
